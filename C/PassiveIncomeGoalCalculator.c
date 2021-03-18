c8ba7bcfd812f22b6d412502ff1a09f35fbdb997
alexxmatei <49484290+alexxmatei@users.noreply.github.com>
2021-03-18 19:27:50 +0200

    Updated program to work with user inputs.

--- C/PassiveIncomeGoalCalculator.c
+++ C/PassiveIncomeGoalCalculator.c
@@ -4,17 +4,23 @@ int main(){
 	#define MONTHS_IN_YEAR 12
 	#define EXPECTED_YEARLY_PASSIVE_INTEREST 0.05
 	#define SUM_MULTIPLIER 1 / EXPECTED_YEARLY_PASSIVE_INTEREST
-	int desiredMonthlyPassiveIncome = 4000;
-	int goalSum = desiredMonthlyPassiveIncome * MONTHS_IN_YEAR * SUM_MULTIPLIER;
+	int desiredMonthlyPassiveIncome = 0;
+	int goalSum = 0;
 	int monthlyIncome = 0;
-	float savingRateMonthly = 0.5; /* 50% */
+	float savingRateMonthly = 0.0f;
 	int savedMonthlyIncome = 0;
 	float yearlyExpectedInterestRate = 0.08; /* 8% */\
 	int savedIncome = 0;
 	
   (void)printf("Enter your monthly income:\n");
   (void)scanf("%d", &monthlyIncome);
+  (void)printf("Enter your percentage saved monthly:\n");
+  (void)scanf("%f", &savingRateMonthly);
+  (void)printf("Enter your desired monthly passive income:\n");
+  (void)scanf("%d", &desiredMonthlyPassiveIncome);
+  savingRateMonthly /= 100;
   savedMonthlyIncome = monthlyIncome * savingRateMonthly;
+  goalSum = desiredMonthlyPassiveIncome * MONTHS_IN_YEAR * SUM_MULTIPLIER;
 	(void)printf("Your goal sum is: %d\n", goalSum);
 	(void)printf("Your savings rate per month is: %f\n", savingRateMonthly * monthlyIncome);
 	int years = 1;
