28c866cd6638cb5c41215a25e5fcab1b2790a315
alexxmatei <49484290+alexxmatei@users.noreply.github.com>
2024-06-04 10:29:25 +0300

    Fix pc_to_pico_serial_com.py

--- embedded/pc_to_pico_serial_com.py
+++ embedded/pc_to_pico_serial_com.py
@@ -16,7 +16,7 @@ while True:
         # Read one character from sys.stdin
         ch = sys.stdin.read(1)
         # Check if the character read is 't'
-        if ch=='ty':
+        if ch=='t':
             # Toggle the state of the LED
             led.value(not led.value())
             # Print a message indicating that the LED has been toggled
